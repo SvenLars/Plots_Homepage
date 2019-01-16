126ced7f51a4a080ea69ba24b735ec0601c31d8e
Sven Hilbert <svenhilbert@wlanhome198089.uni-regensburg.de>
2019-01-16 10:38:51 +0100

    Script

---
+++ Create_Plots.R
@@ -0,0 +1,10 @@
+ggplot(data.frame(
+  Go_1 = c(rnorm(10000), rnorm(10000,4), rnorm(50000,4,3)),
+  Go_2 = c(rnorm(10000,4),rnorm(10000,6), rnorm(50000,-3, 2))
+), aes(x = Go_1, y = Go_2)
+) + 
+  geom_hex() + 
+  scale_fill_distiller(palette= "Spectral", direction=1) +
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+    panel.background = element_blank())
+
